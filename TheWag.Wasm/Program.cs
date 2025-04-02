06823ec66696c5b417656b116ab6705d49e515f6
Brad Rykenhuizen <bradrykenhuizen@gmail.com>
2025-04-02 12:32:28 -0300

    Added class to strongly type AppSettings

--- TheWag.Wasm/Program.cs
+++ TheWag.Wasm/Program.cs
@@ -10,4 +10,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<SessionStorage>();
 
+builder.Services.AddScoped<AppSettings>();
+
 await builder.Build().RunAsync();
---
+++ TheWag.Wasm/Util/AppSettings.cs
@@ -0,0 +1,23 @@
+﻿namespace TheWag.Wasm.Util
+{
+    public class AppSettings
+    {
+        public string BlobHostUrl { get; private set; }
+        public string FunctionHostUrl { get; private set; }
+        public string ContainerName { get; private set; }
+        public string CartSessionKey { get; private set; }
+
+        public AppSettings(IConfiguration configuration)
+        {
+            if (configuration == null)
+            {
+                throw new ArgumentNullException(nameof(configuration));
+            }
+
+            BlobHostUrl = configuration["BlobHostUrl"] ?? throw new ArgumentNullException(nameof(BlobHostUrl));
+            FunctionHostUrl = configuration["FunctionHostUrl"] ?? throw new ArgumentNullException(nameof(FunctionHostUrl));
+            ContainerName = configuration["ContainerName"] ?? throw new ArgumentNullException(nameof(ContainerName));
+            CartSessionKey = configuration["CartSessionKey"] ?? throw new ArgumentNullException(nameof(CartSessionKey));
+        }
+    }
+}
