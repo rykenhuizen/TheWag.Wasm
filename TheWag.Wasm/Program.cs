da182266592c9ca62971a15cef911b0b442741f6
Brad Rykenhuizen <bradrykenhuizen@gmail.com>
2025-04-17 14:34:57 -0300

    more work on app insights

--- TheWag.Wasm/Program.cs
+++ TheWag.Wasm/Program.cs
@@ -5,6 +5,8 @@ using TheWag.Wasm.Services;
 using TheWag.Wasm.Util;
 using System.Globalization;
 using BlazorApplicationInsights;
+using BlazorApplicationInsights.Interfaces;
+using BlazorApplicationInsights.Models;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -22,6 +24,21 @@ var customCulture = (System.Globalization.CultureInfo)System.Globalization.Cultu
 customCulture.NumberFormat.CurrencySymbol = "฿$₭৳";
 CultureInfo.DefaultThreadCurrentCulture = customCulture;
 
-builder.Services.AddBlazorApplicationInsights();
+builder.Services.AddBlazorApplicationInsights(config =>
+{
+    config.ConnectionString = "InstrumentationKey=408010ab-ac53-43a6-a68d-8d4cfe8591dd;IngestionEndpoint=https://canadaeast-0.in.applicationinsights.azure.com/;LiveEndpoint=https://canadaeast.livediagnostics.monitor.azure.com/;ApplicationId=f467e1b2-b5b1-47fa-a26d-3bc35f3a1f36";
+},
+    async applicationInsights =>
+    {
+    var telemetryItem = new TelemetryItem()
+    {
+        Tags = new Dictionary<string, object?>()
+            {
+                { "ai.cloud.role", "SPA" },
+                { "ai.cloud.roleInstance", "Blazor Wasm" },
+             }
+    };
+    await applicationInsights.AddTelemetryInitializer(telemetryItem);
+});
 
 await builder.Build().RunAsync();
